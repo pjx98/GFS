62ddf9c46cd4e92900a60d5ffc80361e5f508d65
PJX <pjingxiang@gmail.com>
2022-03-18 21:06:21 +0800

    added json structures

--- structs/message.go
+++ structs/message.go
@@ -5,6 +5,8 @@ type Message struct {
 	Append_last_chunk string `json:"append_last_chunk"`
 	Source_pid   int    `json:"source_pid"`
 	Target_pid   []int    `json:"target_pid"`
+	// 0 index is the primary chunkserver
+	// 1 and 2 index is the secondary chunkserver
 }
 
 func create_message(message_type string, append_last_chunk string, source_pid int, target_pid []int) Message {
