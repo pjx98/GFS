e613a26e5a5b05cc303e76be984fce92d5000ef7
DPS17 <66048321+DPS17@users.noreply.github.com>
2022-03-20 12:00:26 +0800

    splitFile and change TCP to HTTP (master)

--- Client/client.go
+++ Client/client.go
@@ -8,17 +8,60 @@ import (
 	"net"
 	"strconv"
 	"path/filepath"
+	"math"
+	"strings"
 	//"reflect"
+	"bytes"
+	"io/ioutil"
+	"net/http"
 	"gfs.com/master/helper"
 	structs "gfs.com/master/structs"
+	
 )
+
+// Connect client to master 
+func callMasterAppend(filename string) {
+	var numChunks uint64
+	// debug to check whats the main directory, just leave it here
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(path) 
+
+	// Call helper function to read file size
+	fileByteSize := getFileSize(filename)
+
+	// Check byte size of file, if more than 2.5kb split
+	if(fileByteSize > 2500){
+		numChunks = splitFile(filename)
+	} else{
+		numChunks = 1
+	}
+
+	// If no split, just append as normal
+	// Otherwise append for each file split
+	if numChunks == 1{
+		callAppend(filename, fileByteSize)
+	} else{
+		filePrefix := removeExtension(filename)
+		for i := uint64(0); i < numChunks; i++{
+			smallFile := filePrefix + strconv.FormatUint(i, 10) + ".txt"
+			smallFileSize := getFileSize(smallFile)
+			fmt.Println(smallFile) // debug
+			fmt.Println(smallFileSize) // debug
+
+			//callAppend(smallFile,smallFileSize)
+		}
+	}
+}
 
 // Get size of file trying to write to [Done]
 func getFileSize(filename string) (int64) {
 	
 	// Get relative path of the text file
 	// First arg is the main directory, second arg is where file is stored
-	rel, err := filepath.Rel("GFS/Master", "GFS/Client/test.txt")
+	rel, err := filepath.Rel("GFS/Master", "GFS/Client/" + filename)
     if err != nil {
         panic(err)
     }
@@ -34,74 +77,108 @@ func getFileSize(filename string) (int64) {
 
 }
 
-// Connect client to master [Done]
-func connectMaster(master_port string, filename string) {
+// Used to split files that are larger than 2.5kb [Done]
+func splitFile(oldFilename string)(uint64){
+		
+		// Relative file path
+		rel, err := filepath.Rel("GFS/Master", "GFS/Client/" + oldFilename)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
-	address := "localhost:" + master_port
-	fmt.Println("Master port is " + address)
+		// Open the file
+        file, err := os.Open(rel)
+        if err != nil {
+                fmt.Println(err)
+                os.Exit(1)
+        }
+		defer file.Close()
 
-	// Dial master port == TO-DO: Change tcp to http
-	conn, err := net.Dial("tcp", address)
-	if err != nil {
-		log.Fatalln(err)
-	}
+		// Get file data
+		fileInfo, _ := file.Stat()
+		var fileSize int64 = fileInfo.Size()
+		const fileChunk = 2500 // 2.5 KB
 
-	// debug to check whats the main directory, just leave it here
-	path, err := os.Getwd()
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Println(path) 
+		// calculate total number of parts the file will be chunked into
+		totalPartsNum := uint64(math.Ceil(float64(fileSize) / float64(fileChunk)))
+		fmt.Printf("Splitting to %d pieces.\n", totalPartsNum)
+		for i := uint64(0); i < totalPartsNum; i++ {
 
-	// Call helper function to read file size
-	fileByteSize := getFileSize(filename)
+			partSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
+			partBuffer := make([]byte, partSize)
+			file.Read(partBuffer)
 
+			// write to disk
+			rel, err := filepath.Rel("GFS/Master", "GFS/Client/" + oldFilename)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			rel = removeExtension(rel)
+			fileName := rel + strconv.FormatUint(i, 10) + ".txt"
+			_, err = os.Create(fileName)
+			if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+			}
+			// write/save buffer to disk
+			ioutil.WriteFile(fileName, partBuffer, os.ModeAppend)
+			fmt.Println("Split to : ", fileName)
+		}
+		return totalPartsNum
+}
+
+// Helper to remove extensions [Done]
+func removeExtension(fpath string) string {
+	ext := filepath.Ext(fpath)
+	return strings.TrimSuffix(fpath, ext)
+}
+
+
+func callAppend(filename string, fileByteSize int64){
 	// Create append message json
 	msgJson := &structs.Message{
 		MessageType: helper.DATA_APPEND,
-		Filename:    filename,
+		
+		ClientPort: 8086,
+		PrimaryChunkServer: 0,
+		SecondaryChunkServers: nil,
+		
+		Filename: filename,
+		ChunkId: "",
+		Payload: "",
 		PayloadSize: fileByteSize,
+		ChunkOffset: 0,
+
+		SourcePort: 8086,
+		TargetPorts: nil,
 	}
 
-	data, _ := json.Marshal(msgJson)
-	fmt.Println(string(data)) // debug
+	post, _ := json.Marshal(msgJson)
+	fmt.Println(string(post)) // debug
+	responseBody := bytes.NewBuffer(post)
 
-	//Write append request
-	callAppend(conn, data)
-
-}
-
-// Send APPEND request to master [Done]
-func callAppend(conn net.Conn, req []byte) {
-
-	// write to master port == TO-DO: Change tcp to http
-	_, err := conn.Write(req)
+	// Master port number = 8080
+	resp, err := http.Post("https://localhost:8080/client/append", "application/json", responseBody)
+	
+	//Handle Error
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+	
+	//Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	sb := string(body)
+	log.Printf(sb)
 
-	for {
-		// TO-DO: Change tcp to http
-		buffer := make([]byte, 1400)
-		dataSize, err := conn.Read(buffer)
-		if err != nil {
-			fmt.Println("The connection has closed!")
-			return
-		}
-
-		// Read the reply from master
-		data := buffer[:dataSize]
-		fmt.Println("Received message: ", string(data))
-
-		var reply structs.Message
-		json.Unmarshal(data, &reply)
-		//fmt.Println(reflect.TypeOf(reply)) // debug
-		fmt.Println(reply)
-		break
-	}
 }
 
-// Connect client to chunk servers [Done]
+// Connect client to chunk servers [TO-DO]
 func connectChunks(message structs.Message) {
 
 	for _, s := range message.TargetPorts {
@@ -119,30 +196,30 @@ func connectChunks(message structs.Message) {
 
 
 // parameters: Chunkserver ports, data to send, chunk_id (Append_last_chunk)
-// Sends data to primary chunk and checks for a 
+// Sends data to primary chunk [TO-DO]
 func sendPrimaryChunk(ports []int, data string, chunkID string){
 
 	fmt.Println("here") // debug
 	
 }
 
-// Receive first ACK (data_received) from primary chunk server
+// Receive first ACK (data_received) from primary chunk server [TO-DO]
 func checkFirstACK() {
 
 }
 
-// Send write data signal to primary chunk server
+// Send write data signal to primary chunk server [TO-DO]
 func sendWriteData() {
 
 }
  
-// Receive second ACK from primary chunk server after successful write
+// Receive second ACK from primary chunk server after successful write [TO-DO]
 func checkSuccessWrite() {
 
 }
 
 func StartClient() {
-	connectMaster("8000", "test.txt")
+	callMasterAppend("test.txt")
 }
 
 // func main(){
